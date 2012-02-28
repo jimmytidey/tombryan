29d0a8ecec56cd6861ddfde86e14f550127e198c
jimmytidey <jimmytidey@gmail.com>
2012-02-28 21:53:43 +0000

    copy improvement

--- html/index.php
+++ html/index.php
@@ -50,7 +50,7 @@
 			
 			<p>Available for location recording, studio engineering and production, session bass playing and commissioned compositions. I'm associated with the following organisations:
 				<ul>
-					<li>Technical Director at <a href='http://www.chantrysound.co.uk/'>Chantry Sound</a>, specialising in choral recording.</li>
+					<li>Technical Director at <a href='http://www.chantrysound.co.uk/'>Chantry Sound</a>, specialising in choral recording</li>
 					<li>Freelance engineer at <a href='http://smokehousestudios.co.uk/'>Smokehouse Studios</a></li>
 					<li>Freelance engineer at <a href='http://www.factorystudios.co.uk/'>Factory Studios</a> in Bristol</li>					
 					<li>Installation engineer at the <a href='http://en.wikipedia.org/wiki/Ronnie_Lane%27s_Mobile_Studio'>Ronnie Lane Mobile Studio</a></li>
@@ -64,7 +64,7 @@
 		<div id="tabs-2">
 			<h2>Commercial Recording Engineering</h2>
 			<p>I've recorded numerous performances for commercial and sync at <a href='http://smokehousestudios.co.uk/'>Smokehouse Studios<a>.</p>
-			<p>Smokehouse is a major London studio, clients have included The Kooks, Level 42 and Jack Bruce. The studio also work on soundtracks.</p> 	
+			<p>Smokehouse is a major London studio, clients included The Kooks, Level 42 and Jack Bruce. I've also worked on film soundtracks at Smokehouse, such as <em>Son of Rambow</em>.</p> 	
 				
 		
 			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F32801&show_artwork=false"></iframe>
@@ -74,8 +74,8 @@
 		
 		<div id="tabs-3">
 			<h2>Location Recording</h2>
-			<p>I'm technical manager at <a href='http://www.chantrysound.co.uk/'>Chantry Sound</a> which makes on-site classical and organ recordings. I also record bands and soloists in their homes, or in locations that have been chosen for their acoustics.</p>
-			<p>My equipment allows me to make excellent recordings in challenging locations:</p>
+			<p>I'm technical manager at <a href='http://www.chantrysound.co.uk/'>Chantry Sound</a>. Chantry Sound make on-site classical and organ recordings. I also record bands and soloists in their homes or in locations chosen for their acoustics.</p>
+			<p>I have my own recording equipment which allows me to make excellent recordings in challenging locations:</p>
 			<ul>
 				<li>Macbook Pro/ Logic Pro/ Motu 896 MK3 / 8pre</p>
 				<li>Mics from AKG, Shure, Nakamichi, SE</p>
@@ -87,13 +87,13 @@
 		
 		<div id="tabs-4">
 			<h2>Engineering &amp; Production</h2>
-			<p>I've worked as a producer and engineer in a number of studios. I'm currently working with <a href='http://www.factorystudios.co.uk/'>Factory Studios</a>, one of the largest recording locations in Bristol, to develop their recording facilities.</p> 
+			<p>I've worked as a producer and engineer in a number of studios. I'm currently working with <a href='http://www.factorystudios.co.uk/'>Factory Studios</a>, one of the largest recording locations in Bristol developing their recording facilities.</p> 
 			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F21940008&show_artwork=false"></iframe>
 		</div>
 		
 		<div id="tabs-5">
 			<h2>Commissioned compositions</h2>
-			<p>I compose and record compositions to brief in my home studio. My previous work has focussed on theatre and dance, and I regularly work with <a href='http://chimaerafirecrew.co.uk/'>Chimaera Fire Crew</a>.</p>
+			<p>I compose and record pieces to brief in my home studio. My previous work has focussed on theatre and dance, and I regularly work with <a href='http://chimaerafirecrew.co.uk/'>Chimaera Fire Crew</a>.</p>
 		
 			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F15580800&show_artwork=false"></iframe>		
 			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F21515006&show_artwork=false"></iframe>
@@ -101,9 +101,11 @@
 		
 		<div id="tabs-6">
 			<h2>Contact Me</h2>
-
+			Email: tb1983 at googlemail dot com 
+			
+		<br/><br/>
 			<? include('contact-form.php'); ?>
-			</center>
+			
 		</div>	
 	</div>
 	
